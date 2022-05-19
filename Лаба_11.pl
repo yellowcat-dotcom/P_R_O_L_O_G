0d38215792867547606557e00352c77f574d28eb
Валентина Гончаренко <83706757+yellowcat-dotcom@users.noreply.github.com>
2022-05-19 13:19:42 +0300

    Add files via upload

---
+++ Лаба_11.pl
@@ -0,0 +1,56 @@
+%Семьи, из файла А.С.
+man(voeneg).
+man(ratibor).
+man(boguslav).
+man(velerad).
+man(duhovlad).
+man(svyatoslav).
+man(dobrozhir).
+man(bogomil).
+man(zlatomir).
+
+woman(goluba).
+woman(lubomila).
+woman(bratislava).
+woman(veslava).
+woman(zhdana).
+woman(bozhedara).
+woman(broneslava).
+woman(veselina).
+woman(zdislava).
+
+parent(voeneg,ratibor).
+parent(voeneg,bratislava).
+parent(voeneg,velerad).
+parent(voeneg,zhdana).
+
+parent(goluba,ratibor).
+parent(goluba,bratislava).
+parent(goluba,velerad).
+parent(goluba,zhdana).
+
+parent(ratibor,svyatoslav).
+parent(ratibor,dobrozhir).
+parent(lubomila,svyatoslav).
+parent(lubomila,dobrozhir).
+
+parent(boguslav,bogomil).
+parent(boguslav,bozhedara).
+parent(bratislava,bogomil).
+parent(bratislava,bozhedara).
+
+parent(velerad,broneslava).
+parent(velerad,veselina).
+parent(veslava,broneslava).
+parent(veslava,veselina).
+
+parent(duhovlad,zdislava).
+parent(duhovlad,zlatomir).
+parent(zhdana,zdislava).
+parent(zhdana,zlatomir).
+
+%Задание_11
+% Построить предикат son(X, Y), который проверяет, является ли X сыном Y.
+% Построить предикат, son(X), который выводит сына X.
+son(X,Y):-parent(Y,X), man(X).
+son(X):-parent(X,Y),son(Y,X),print(Y),nl,fail.
