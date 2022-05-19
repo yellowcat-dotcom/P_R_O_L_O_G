b7795eaff557a2cc50bfb246b07493c360cb69d1
Валентина Гончаренко <83706757+yellowcat-dotcom@users.noreply.github.com>
2022-05-19 13:31:36 +0300

    Add files via upload

--- Лаба_12.pl
+++ Лаба_12.pl
@@ -11,3 +11,38 @@ kolDelNot3_UP(X,0,K):-!, K is 0.
 kolDelNot3_UP(X,Z,K):-
      N_Z is Z-1,
     (0 is X mod Z, 0 =\= Z mod 3 -> kolDelNot3_UP(X,N_Z,Kol), K is Kol+1; kolDelNot3_UP(X,N_Z,K)),!.
+
+
+
+%Задание_12
+%Найти сумму всех делителей числа, взаимно простых с суммой цифр числа и не взаимно простых с произведением цифр числа.
+% 1 - найти произведение чисел
+% 2 - сумму цифр числа НЕ ВЗАИМНО ПРОСТЫХ С 1 НОД=1
+% 3 - сумму цифр числа ВЗАИМНО ПРОСТЫХ С 2
+pr(X,Y):-pr(X,Y,1).     %Произведение
+pr(0,Y,P):-!,Y is P.
+pr(X,Y,P):-
+     X1 is X mod 10,
+     N_X is X div 10,
+     N_P is X1*P,
+     pr(N_X,Y,N_P).
+%НОД
+nod(X,X,Z):-!,Z is X.
+nod(X,Y,Z):-
+     N_x is X-Y,
+     N_y is Y-X,
+     (X>Y->nod(N_x,Y,Z);nod(X,N_y,Z)).
+
+sum(X,Y):- pr(X,P_const),sum(X,Y,0,P_const).
+sum(0,Y,S,P_const):-!,Y is S.
+sum(X,Y,S,P_const):-
+     X1 is X mod 10,
+     N_X is X div 10,
+     nod(X1,P_const,K),
+     (1=\=K->N_S is S+X1,sum(N_X,Y,N_S,P_const);sum(N_X,Y,S,P_const)).
+
+summ(X,Y):-sum(X,SS),summ(X,X,Y,0,SS).   %Итоговый предикат
+summ(X,0,Y,S,SS_const):-!,Y is S.
+summ(X,X2,Y,S,SS_const):-
+     NX2 is X2-1,
+     (0 is X mod X2, nod(SS_const,X2,K), 1 is K -> N_S is S+X2, summ(X,NX2,Y,N_S,SS_const);summ(X,NX2,Y,S,SS_const)).
