37db70cbea817813eb3dc0982c185f87a9adb78d
Валентина Гончаренко <83706757+yellowcat-dotcom@users.noreply.github.com>
2022-05-28 07:49:11 +0300

    Add files via upload

--- Лаба_13.pl
+++ Лаба_13.pl
@@ -25,3 +25,42 @@ task11:-
     readList(N,L),
     meanArithmetic(L,B),
     print(B).
+
+%«адача_12
+%ƒл€ введенного списка положительных чисел построить список всех по-ложительных простых делителей элементов списка без повторений.
+append([],X,X).
+append([X|T],Y,[X|T1]):- append(T,Y,T1).
+
+prost(X,Y):-Nx is X-1,prost(X,Nx,Y,0).
+prost(_,1,Z,Z):-!.
+prost(X,Nx,Y,Z):-
+    Nnx is Nx-1,
+    (X mod Nx =:= 0 ->(Nz is Z+1,prost(X,Nnx,Y,Nz));prost(X,Nnx,Y,Z)).
+
+spisokD(X,List):-spisokD(X,X,List,[]).
+spisokD(X,1,List,List):-!.
+spisokD(X,Nx,ListO,List):-
+    Nnx is Nx-1,
+    0 is X mod Nx, prost(Nx,A),0 is A, append(List,[Nx],C),spisokD(X,Nnx,ListO,C).
+spisokD(X,Nx,ListO,List):-
+    Nnx is Nx-1,spisokD(X,Nnx,ListO,List).
+
+
+in_list([El|_],El).
+in_list([_|T],El):-in_list(T,El).
+
+unicList(List,List2):- unicList(List,List2,[]).  %лист без повторов
+unicList([],List,List):-!.
+unicList([H|T],List2,ListNow) :- in_list(ListNow,H), unicList(T,List2,ListNow),!;
+    append(ListNow,[H],NewList), unicList(T,List2,NewList).
+
+spisokX([H|T],List):-spisokX([H|T],List,[]).
+spisokX([],List,List):-!.
+spisokX([H|T],ListO,List):-spisokD(H,L),append(List,L,C),spisokX(T,ListO,C).
+
+task12:-
+    read(N),
+    readList(N,L),
+    spisokX(L,L1),
+    unicList(L1,L2),
+    write(L2).
